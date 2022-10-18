98b011c41ea27abce051a78127a6441f0f146c2a
Sam Farber <sfarber@obtainmedrecs.com>
2022-10-18 12:34:44 -0400

    Through Module 11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -19,3 +19,20 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("datetime").property("value");
+    let filteredData = tableData;
+    // Check to see if a date was entered and filter the data using that date.
+    if (date) {
+        // Apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild table using filtered data. @NOTE if no date was entered, filtered data will be the original tableData.
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
